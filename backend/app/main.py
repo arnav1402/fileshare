24c7178b3d8e43eb87f5e46d764c8976ce818460
arnav1402 <12.ferreira.arnav@gmail.com>
2026-04-28 23:25:46 +0530

    frontend url + ngrock

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://file-sharev2.vercel.app", "https://oyster-untamed-kite.ngrok-free.dev "],  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
